e17ae2f2f1abcf09b0d1c7a2fb2d8ec532722e60
Stephen Zoio <stephen.zoio@zendesk.com>
2020-09-13 00:35:35 +1000

    test fix

--- controllers/permissions_test.go
+++ controllers/permissions_test.go
@@ -35,7 +35,7 @@ var _ = Describe("Test permissions", func() {
 			Expect(record.Events).To(Not(ContainElement(manager.EventCreate)))
 
 			object, _ := getObjectA(key)
-			Expect(object.Status.Message).To(HavePrefix("permission to create resource is not set"))
+			Expect(object.Status.Message).To(HavePrefix("permission to create external resource is not set"))
 		})
 
 		It("should update if update if permission is present", func() {
